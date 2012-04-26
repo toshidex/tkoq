e4b45587db1cf0d60a9f172e4d18dd7c07b5c28f
toshidex <lightuono@gmail.com>
2012-04-26 17:44:13 +0200

    Create the function shuffle_cards

--- tkoq.sh
+++ tkoq.sh
@@ -13,10 +13,14 @@
 blue="\e[0;1;34m"
 green="\e[0;1;32m"
 red="\e[0;1;31m"
+white="\e[0;1;39m"
 gray="\e[0;34m"
 lgreen="\e[0;33m"
 rosa="\e[0;31m"
 rstclr="\e[m"
+queen="$blue┏━┓\n┃┓┃\n$rstclr$gray┗┻┛$rstclr"
+king="$blue╻┏\n┣┻┓$rstclr\n$gray╹ ╹$rstclr"
+
 cards=( " $blue┏━┓ \t ╻┏ \t ╻┏ $rstclr
  $blue┃┓┃ \t ┣┻┓ \t ┣┻┓ $rstclr
  $gray┗┻┛ \t ╹ ╹ \t ╹ ╹ $rstclr"
@@ -30,9 +34,9 @@ cards=( " $blue┏━┓ \t ╻┏ \t ╻┏ $rstclr
  $gray╹ ╹ \t ╹ ╹ \t ┗┻┛ $rstclr"
 
 #CARDS FACE DOWN#
-"$blue╻ ╻ \t ╻ ╻ \t ╻ ╻ $rstclr
-$blue ╺╋╸ \t ╺╋╸ \t ╺╋╸ $rstclr
-$gray ╹ ╹ \t ╹ ╹ \t ╹ ╹ $rstclr"
+"$white╻ ╻ \t ╻ ╻ \t ╻ ╻ $rstclr
+$white ╺╋╸ \t ╺╋╸ \t ╺╋╸ $rstclr
+$white ╹ ╹ \t ╹ ╹ \t ╹ ╹ $rstclr"
 
 #CARDS LOSE#
 "$red┏━┓$rstclr \t $blue╻┏ \t ╻┏ $rstclr
@@ -70,8 +74,8 @@ load_tkoq() {
 	echo -e "*******************************************"
 	echo -e "\nWelcome to Tkoq [ Two King One Queen ]"
 	echo -e "\n* Istructions: "
-	echo -e "${cards[0]} == Queen Cards"
-	echo -e "${cards[1]} == King Cards"
+	echo -e "$queen == Queen Cards"
+	echo -e "$king == King Cards"
 	
 	echo -e "*******************************************"
 	echo -e "\nPress a key for continue..."
@@ -80,15 +84,35 @@ load_tkoq() {
 	[[ -z $key ]] && return 100
 }
 
+
+shuffle_cards(){
+	
+	clear
+	echo -e "${cards[0]}"
+	sleep 0.2
+	clear
+	echo -e " ${cards[1]}"
+	sleep 0.2
+	clear
+	echo -e " ${cards[2]}"
+	sleep 0.2
+
+}
+
 prestart_tkoq() {
 	
 	clear
-	echo -e "*******************************************"
-	echo -e "${cards[0]} \n ${cards[1]} \n ${cards[2]} \n ${cards[3]} \n ${cards[4]} \n ${cards[5]} \n ${cards[6]} \n ${cards[7]} \n ${cards[8]} \n ${cards[9]}" 
-
-
-
-
+	echo -e "There are three cards..."
+	echo -e "\n ${cards[3]}"
+	echo -e "\nAre you ready to play? [y/n]"
+	read -s -n 1 key
+	
+	if [[ $key == "y" || $key == "Y" ]]; then
+		shuffle_cards
+	else
+		exit 0
+	fi
+#	echo -e "${cards[0]} \n ${cards[1]} \n ${cards[2]} \n ${cards[3]} \n ${cards[4]} \n ${cards[5]} \n ${cards[6]} \n ${cards[7]} \n ${cards[8]} \n ${cards[9]}" 
 }
 
 
