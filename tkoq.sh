a8d658986b73bcad4390dfc85d7f3a4fffef64f2
toshidex <lightuono@gmail.com>
2012-04-26 13:02:27 +0200

    Set cards complete

--- tkoq.sh
+++ tkoq.sh
@@ -11,8 +11,10 @@
 
 #Init Variables
 blue="\e[0;1;34m"
+green="\e[0;1;32m"
 red="\e[0;1;31m"
 gray="\e[0;34m"
+lgreen="\e[0;33m"
 rosa="\e[0;31m"
 rstclr="\e[m"
 cards=( " $blue┏━┓ \t ╻┏ \t ╻┏ $rstclr
@@ -27,10 +29,12 @@ cards=( " $blue┏━┓ \t ╻┏ \t ╻┏ $rstclr
  $blue┣┻┓ \t ┣┻┓ \t ┃┓┃ $rstclr
  $gray╹ ╹ \t ╹ ╹ \t ┗┻┛ $rstclr"
 
+#CARDS FACE DOWN#
 "$blue╻ ╻ \t ╻ ╻ \t ╻ ╻ $rstclr
 $blue ╺╋╸ \t ╺╋╸ \t ╺╋╸ $rstclr
 $gray ╹ ╹ \t ╹ ╹ \t ╹ ╹ $rstclr"
 
+#CARDS LOSE#
 "$red┏━┓$rstclr \t $blue╻┏ \t ╻┏ $rstclr
  $red┃┓┃$rstclr \t $blue┣┻┓ \t ┣┻┓ $rstclr
  $rosa┗┻┛$rstclr \t $gray╹ ╹ \t ╹ ╹ $rstclr"
@@ -42,6 +46,19 @@ $gray ╹ ╹ \t ╹ ╹ \t ╹ ╹ $rstclr"
 "$blue╻┏ \t ╻┏$rstclr \t $red┏━┓ $rstclr
  $blue┣┻┓ \t ┣┻┓$rstclr \t $red┃┓┃ $rstclr
  $gray╹ ╹ \t ╹ ╹$rstclr \t $rosa┗┻┛ $rstclr"
+
+#CARDS VICTORY#
+"$green┏━┓$rstclr \t $blue╻┏ \t ╻┏ $rstclr
+ $green┃┓┃$rstclr \t $blue┣┻┓ \t ┣┻┓ $rstclr
+ $lgreen┗┻┛$rstclr \t $gray╹ ╹ \t ╹ ╹ $rstclr"
+
+"$blue╻┏$rstclr \t $green┏━┓$rstclr \t $blue╻┏ $rstclr
+ $blue┣┻┓$rstclr \t $green┃┓┃$rstclr \t $blue┣┻┓ $rstclr
+ $gray╹ ╹$rstclr \t $lgreen┗┻┛$rstclr \t $gray╹ ╹ $rstclr"
+
+"$blue╻┏ \t ╻┏$rstclr \t $green┏━┓ $rstclr
+ $blue┣┻┓ \t ┣┻┓$rstclr \t $green┃┓┃ $rstclr
+ $gray╹ ╹ \t ╹ ╹$rstclr \t $lgreen┗┻┛ $rstclr"
 )
 
 
@@ -67,7 +84,7 @@ prestart_tkoq() {
 	
 	clear
 	echo -e "*******************************************"
-	echo -e "${cards[0]} \n ${cards[1]} \n ${cards[2]} \n ${cards[3]} \n ${cards[4]} \n ${cards[5]} \n ${cards[6]}" 
+	echo -e "${cards[0]} \n ${cards[1]} \n ${cards[2]} \n ${cards[3]} \n ${cards[4]} \n ${cards[5]} \n ${cards[6]} \n ${cards[7]} \n ${cards[8]} \n ${cards[9]}" 
 
 
 
